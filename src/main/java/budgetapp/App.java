32f9278a3b6ea86c4dd60bc5e516d5f4eb702bb7
ajja99 <aja_99@hotmail.com>
2022-09-14 08:16:09 +0200

    Add: default maven functionality

---
+++ src/main/java/budgetapp/App.java
@@ -0,0 +1,15 @@
+package budgetapp;
+
+public class App {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World!");
+
+    }
+
+    public void print() {
+        System.out.println("1");
+    }
+
+}
