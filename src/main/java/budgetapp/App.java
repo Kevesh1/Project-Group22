3a44c67b96ad1c9d7149cbca2ec5bc0faf387596
ajja99 <aja_99@hotmail.com>
2022-09-14 22:46:50 +0200

    fix: dependencies for jUnit with maven

--- src/main/java/budgetapp/App.java
+++ src/main/java/budgetapp/App.java
@@ -1,10 +1,37 @@
 package budgetapp;
 
-public class App {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class App extends Application {
 
     public static void main(String[] args){
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
 
-        System.out.println("Hello World!!");
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
 
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
     }
 }
--- src/test/java/budgetapp/AppTest.java
+++ src/test/java/budgetapp/AppTest.java
@@ -1,4 +1,19 @@
 package budgetapp;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class AppTest {
+    @Test
+    public void tempTest() {
+        assertTrue(true);
+    }
+    @Test
+    public void tempTest2() {
+        fail();
+    }
 }
