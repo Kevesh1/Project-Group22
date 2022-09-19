c663723f46087751300ba967b122afced3a80427
jacob <jacob.s.westerberg@gmail.com>
2022-09-19 10:38:28 +0200

    More changes to these classes

--- src/main/java/budgetapp/model/FrontPage.java
+++ src/main/java/budgetapp/model/FrontPage.java
@@ -3,23 +3,35 @@ import java.util.*;
 
 public class FrontPage {
     private int numUsers;
-    private String[] Users;
+    private List<User> usersList;
+    private int maxNumUsers;
 
 
 
     public FrontPage(int numUsers, String[] Users){
         this.numUsers = numUsers;
         this.Users = Users;
+        this.maxNumUsers = 4;
     }
 
-    public static void controlNumUsers(String[] Users){
-        if (size().Users =< 4){
+    public int getNumUsers(){
+        return numUsers;
+    }
+
+    public int getMaxNumUsers(){
+        return maxNumUsers;
+    }
+
+    public void calculateNumUsers(String[] usersList){
+        int numUsers = usersList.size();
+    }
+
+
+    public void userCreate(){
+        calculateNumUsers(String[] usersList);
+        if (getNumUsers() <= getNumUsers()){
 
         }
-    }
-
-    public static void userCreate(){
-        User User;
 
     }
 }
