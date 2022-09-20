8ed7772fad065fca1d284b258cb3ae7129698d62
DSkretting <david.skretting@gmail.com>
2022-09-20 10:04:06 +0200

    Add: class expenseAndIncomeController

---
+++ src/main/java/budgetapp/controller/expenseAndIncomeController.java
@@ -0,0 +1,20 @@
+package budgetapp.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Circle;
+
+public class expenseAndIncomeController {
+
+    @FXML
+    private TextField sumwindow;
+    @FXML
+    private TextField note;
+    @FXML
+    private DatePicker datePicker;
+    @FXML
+    private Circle swapCategory;
+
+
+}
