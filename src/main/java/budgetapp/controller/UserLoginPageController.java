e2a87e78df9f7901647256c79bc2bea6386aed45
jacob <jacob.s.westerberg@gmail.com>
2022-10-10 11:51:20 +0200

    FIX: Tried fusing johannes and my classes that are doing the same thing.

--- src/main/java/budgetapp/controller/UserLoginPageController.java
+++ src/main/java/budgetapp/controller/UserLoginPageController.java
@@ -1,15 +1,18 @@
 package budgetapp.controller;
 
 import budgetapp.App;
+import budgetapp.model.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
 
-public class UserLoginPageController {
+public class UserLoginPageController extends AnchorPane {
 
     @FXML
     private Button returnFrontButton;
@@ -21,8 +24,9 @@ public class UserLoginPageController {
     private PasswordField passwordField;
 
 
-    public void userLogin(ActionEvent event) throws IOException{
+    public void userLogin(ActionEvent event, User user) throws IOException{
         passwordValidity();
+        createMainView(user);
     }
 
     public void passwordValidity() throws IOException{
@@ -33,9 +37,17 @@ public class UserLoginPageController {
 
     }
 
+    public void createMainView(User user) {
+        MainController mainController = new MainController(user);
+        this.getScene().setRoot(mainController);
+    }
+
+
     public void returnToFrontPage(ActionEvent event) throws IOException{
         App app = new App();
         app.changeScene("FrontPage.fxml");
 
     }
+
+
 }
