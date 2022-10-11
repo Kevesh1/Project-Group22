487bb50fa4ad2c9f184b48afc37fd29e507488ee
jacob <jacob.s.westerberg@gmail.com>
2022-10-11 09:35:15 +0200

    FIX: Removing obsolete classes

--- src/main/java/budgetapp/controller/expenseAndIncomeController.java
+++ src/main/java/budgetapp/controller/expenseAndIncomeController.java
@@ -14,51 +14,6 @@ import javafx.util.Duration;
 
 public class expenseAndIncomeController {
 
-    @FXML
-    private TextField sumWindow;
-    @FXML
-    private TextArea note;
-    @FXML
-    private DatePicker transactionDate;
-    @FXML
-    private Button categorySwap;
-    @FXML
-    private AnchorPane categorySelect;
-    @FXML
-    private AnchorPane SwapCategory;
-    @FXML
-    private ImageView closeButton;
-    @FXML private VBox statusContainer;
-    @FXML
-    private Button close;
-
-
-
-    private TranslateTransition showStatus;
-    private TranslateTransition hideStatus;
-    private boolean showsStatus = false;
-
-    @FXML void initialize() {
-
-        showStatus = new TranslateTransition(Duration.millis(250), statusContainer);
-        showStatus.setByY(-100.0);
-        showStatus.setOnFinished(event -> showsStatus = true);
-        hideStatus = new TranslateTransition(Duration.millis(250), statusContainer);
-        hideStatus.setByY(100.0);
-        hideStatus.setOnFinished(event -> showsStatus = false);
-    }
-
-    public void toggleStatus() {
-        if( showsStatus ) {
-            showStatus.stop();
-            hideStatus.play();
-        }
-        else {
-            hideStatus.stop();
-            showStatus.play();
-        }
-    }
-
 }
 
 
--- src/main/java/budgetapp/model/expenseAndIncome.java
+++ src/main/java/budgetapp/model/expenseAndIncome.java
@@ -2,21 +2,4 @@ package budgetapp.model;
 
 public class expenseAndIncome {
 
-    private String sum;
-
-    private String note;
-
-
-    public expenseAndIncome(String sum, String Note){
-        this.sum =sum;
-        this.note = note;
-    }
-
-    public String getSum() {
-        return sum;
-    }
-
-    public String getNote() {
-        return note;
-    }
 }
