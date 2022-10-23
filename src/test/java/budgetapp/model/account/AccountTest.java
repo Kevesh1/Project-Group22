c14528d10eb1b528138ad728b0c7e08bf32bbad2
jacob <jacob.s.westerberg@gmail.com>
2022-10-23 12:45:26 +0200

    ADD: New tests to AccountTest

--- src/test/java/budgetapp/model/account/AccountTest.java
+++ src/test/java/budgetapp/model/account/AccountTest.java
@@ -3,6 +3,7 @@ package budgetapp.model.account;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,12 +12,13 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AccountTest {
 
     private Account account;
+    private User user;
+    private User user2;
 
     @BeforeEach
     void setUp(){
         this.account = new Account("Jacob Westerberg", "BraMedGlass17");
         this.account.setId("3");
-        User user1 = new User("Jake", "HomeBome55", "mario");
 
     }
 
@@ -26,8 +28,21 @@ public class AccountTest {
     }
 
     @Test
+    void setUsername(){
+        account.setUsername("Haj Bo");
+        assertEquals("Haj Bo", account.getUsername());
+    }
+
+    @Test
     void getPassword(){
         assertEquals("BraMedGlass17", account.getPassword());
+    }
+
+
+    @Test
+    void setPassword(){
+        account.setPassword("gladIDAG55");
+        assertEquals("gladIDAG55", account.getPassword());
     }
 
     @Test
@@ -35,14 +50,51 @@ public class AccountTest {
         assertEquals("3", account.getId());
     }
 
-    /*
+
     @Test
-    void getUsers() {
-        assertEquals("Jake", account.getUsers().get(1).getUsername());
+    void setUsers() {
+        this.user = new User("Jacob Westerberg", "OMG2012again", "mario");
+        this.user.setId("1");
+        this.user.setFirstName("Bengt");
+        this.user.setLastName("Ingvar");
+        this.user.setElderlyAdjusted(false);
+        this.user.setFamilyShare(false);
+        List<User> userList = new ArrayList<>();
+        userList.add(user);
+        account.setUsers(userList);
+        assertEquals(userList, account.getUsers());
 
     }
-    
-     */
+
+    @Test
+    void setMultipleUsers(){
+        this.user = new User("Jacob Westerberg", "OMG2012again", "mario");
+        this.user.setId("1");
+        this.user.setFirstName("Bengt");
+        this.user.setLastName("Ingvar");
+        this.user.setElderlyAdjusted(false);
+        this.user.setFamilyShare(false);
+        this.user2 = new User("Jake West", "OMG2013again", "luigi");
+        this.user2.setId("2");
+        this.user2.setFirstName("Benge");
+        this.user2.setLastName("Inge");
+        this.user2.setElderlyAdjusted(false);
+        this.user2.setFamilyShare(false);
+        List<User> userList = new ArrayList<>();
+        userList.add(user);
+        userList.add(user2);
+        account.setUsers(userList);
+        assertEquals(userList, account.getUsers());
+
+    }
+
+    @Test
+    void getUsers() {
+        assertEquals(null, account.getUsers());
+
+    }
+
+
 
     @Test
     void accountToStringTest(){
